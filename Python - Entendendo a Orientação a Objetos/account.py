0abf7c25b931d1ec294b9a1e7a02d032f608145f
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-12-14 11:31:13 -0300

    feat: encapsulamento

--- Python - Entendendo a Orientação a Objetos/account.py
+++ Python - Entendendo a Orientação a Objetos/account.py
@@ -1,37 +1,38 @@
 class Account:
     def __init__(self, code, owner, balance, limit):
-        self.code = code
-        self.owner = owner
-        self.balance = balance
-        self.limit = limit
+        self.__code = code
+        self.__owner = owner
+        self.__balance = balance
+        self.__limit = limit
 
     def deposit(self, value):
-        self.balance += value
-        print("Deposit of ${} done".format(value))
+        self.__balance += value
+        print("\nDeposit of ${} done".format(value))
 
     def withdraw(self, value):
-        if(self.balance + self.limit < value):
-            print("The value {} is greater than the limit".format(value))
-            print("Balance: {} Limit: {}".format(self.balance, self.limit))
+        if(self.__balance + self.__limit < value):
+            print("\nThe value {} is greater than the limit".format(value))
+            print("Balance: {} Limit: {}".format(self.__balance, self.__limit))
             return False
         else:
-            self.balance -= value
-            print("Withdraw of ${} done".format(value))
-            print("Balance: {} Limit: {}".format(self.balance, self.limit))
+            self.__balance -= value
+            print("\nWithdraw of ${} done".format(value))
+            print("Balance: {} Limit: {}".format(self.__balance, self.__limit))
             return True
         
     def statement(self):
-        print("Hello, {}!".format(self.owner))
-        print("Code: {} Balance: {} Limit: {}".format(self.code, self.balance, self.limit))
+        print("\nHello, {}!".format(self.__owner))
+        print("Code: {} Balance: {} Limit: {}".format(self.__code, self.__balance, self.__limit))
+
+    def transfer(self, value, destiny):
+        self.withdraw(value)
+        destiny.deposit(value)
+        print("\nTransfer of ${} done from {} to {}".format(value, self.__owner, destiny.__owner))
 
 account = Account(123, "Nico", 55.0, 1000.0)
-account.deposit(100.0)
-account.withdraw(50.0)
+account2 = Account(321, "Marco", 100.0, 1000.0)
 account.statement()
-account.withdraw(10000.0)
-account.withdraw(1000.0)
+account2.statement()
+account.transfer(10.0, account2)
 account.statement()
-
-from date_formatter import Date
-date = Date(14, 10, 2000)
-date.format()
+account2.statement()
