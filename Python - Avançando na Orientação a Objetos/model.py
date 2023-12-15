5ca87928dd19bf2724b431592a09347f0a219a8f
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-12-15 17:46:07 -0300

    feat: implementação de herança

--- Python - Avançando na Orientação a Objetos/model.py
+++ Python - Avançando na Orientação a Objetos/model.py
@@ -1,8 +1,7 @@
-class Movie:
-    def __init__(self, title, year, duration, likes = 0):
+class Midia:
+    def __init__(self, title, year, likes = 0):
         self.__title = title.title()
         self.year = year
-        self.duration = duration
         self.__likes = likes
 
     def __str__(self):
@@ -22,40 +21,33 @@ class Movie:
 
     def like(self):
         self.__likes += 1
+
+
+class Movie (Midia):
+    def __init__(self, title, year, duration, likes = 0):
+        super().__init__(title, year, likes)
+        self.duration = duration
+
+    def __str__(self):
+        return f'{self.title} - {self.year} - {self.duration} minutes'
+    
+class Series (Midia):
+    def __init__(self, title, year, seasons, likes = 0):
+        super().__init__(title, year, likes)
+        self.seasons = seasons
+
+    def __str__(self):
+        return f'{self.title} - {self.year} - {self.seasons} seasons'
     
 
 movie = Movie('reservoir dogs', 1992, 99)
 movie.like()
-print(f'{movie} - {movie.duration} minutes')
+print(movie)
 print(f'{movie.likes} likes')
 
-class Series:
-    def __init__(self, title, year, seasons, likes = 0):
-        self.__title = title.title()
-        self.year = year
-        self.seasons = seasons
-        self.__likes = likes
-
-    def __str__(self):
-        return f'{self.__title} - {self.year}'
-    
-    @property
-    def title(self):
-        return self.__title
-    
-    @title.setter
-    def title(self, title):
-        self.__title = title
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def like(self):
-        self.__likes += 1
 
 series = Series('atlanta', 2018, 3)
 series.like()
 series.like()
-print(f'{series} - {series.seasons} seasons')
+print(series)
 print(f'{series.likes} likes')
