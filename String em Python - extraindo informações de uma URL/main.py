d25587f22cce6c23f85a078c7a14145d3a016af6
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-12-21 11:05:30 -0300

    feat: métodos de string

--- String em Python - extraindo informações de uma URL/main.py
+++ String em Python - extraindo informações de uma URL/main.py
@@ -1,7 +1,17 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
 
-base_url = url[0:19]
-print(base_url)
-
-url_params = url[20:36]
+# Separando a base da URL dos parâmetros
+url_params = url[url.find("?")+1:]
 print(url_params)
+
+# Buscando o valor de um parâmetro
+search_param = "moedaDestino"
+index_param = url_params.find(search_param)
+index_value = index_param + len(search_param) + 1
+end_index = url_params.find("&", index_value)
+
+if end_index == -1:
+    end_index = len(url_params)
+value = url_params[index_value:end_index]
+
+print(value)
